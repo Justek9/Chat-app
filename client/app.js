a0f88da85353987beb6456410f5717526ac90437
Justyna Konikiewicz <zagorskaj@wp.pl>
2023-10-11 10:41:44 +0200

    add functionality to login button

--- client/app.js
+++ client/app.js
@@ -7,3 +7,19 @@ const messagesList = document.querySelector('#messages-list')
 const addMessageForm = document.querySelector('#add-messages-form')
 const userNameInput = document.querySelector('#username')
 const messageContentInput = document.querySelector('#message-content')
+
+let userName
+
+const login = function (e) {
+	e.preventDefault()
+	if (!userNameInput.value) {
+		alert('Please add user name')
+	} else {
+		userName = userNameInput.value
+		loginForm.classList.remove('show')
+		messagesSection.classList.add('show')
+		return userName
+	}
+}
+
+loginForm.addEventListener('submit', login)
